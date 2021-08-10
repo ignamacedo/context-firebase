f46b8545b8e219cbb41bd967ef04e2027cdd941e
ignamacedo <ignaciomacedo1@gmail.com>
2021-08-10 15:26:31 -0300

    Commit Desafio 10

--- src/components/items/ItemDetailContainer.js
+++ src/components/items/ItemDetailContainer.js
@@ -26,67 +26,17 @@ function ItemDetailContainer(){
         if(mostrarBtn){
             setMostrarBtn(!mostrarBtn);
         }
-    } 
- 
-    /*const getProduct = async () =>{
-        let data = await fetch(`http://localhost:4000/product/${itemID}`);
-        const responseData = await data.json();
-        return new Promise((res, rej) => {
-        setTimeout(() => {
-            res(setProduct(responseData));
-        },2000);
-        });
-    }*/
-
-    /*useEffect(() => {
-        const firestore = getFireStore();
-        //console.log(firestore);
-        const col = firestore.collection("ItemCollection");
-        //console.log(col);
-        const condicion = col.where('id', '==', 3); //itemID
-        //console.log(condicion);
-        condicion.get().then((qs) => {
-            if(qs.size === 0){
-                console.log('vacio');
-            }
-            setProduct(qs.docs.map(doc => doc.data())); 
-        })
-    },[]);*/
-
-    /*useEffect(() => {
-        //getProduct();
-        const firestore = getFireStore();
-        //console.log(firestore);
-        const collection = firestore.collection("ItemCollection");
-        //console.log(collection);
-        const condicion = collection.where('id', '==', 4); //itemID
-        //console.log(condicion);
-        const query = condicion.get();
-        //console.log(query);
-        query.then((res)=>{
-            res.forEach(doc => {
-            product.push(doc.data());
-            setProduct([...product]); 
-            //console.log(doc.data());
-          });
-        });
-    },[]);*/
+    }
 
     useEffect(()=>{
-        //getItems();
         const firestore = getFireStore();
-        //console.log(firestore);
         const collection = firestore.collection("ItemCollection");
-        //console.log(collection);
         const query = collection.get(); 
         query.then((resultado)=>{
-          //console.log(resultado);
-          //console.log(resultado.docs);
           resultado.forEach(documento => {
               if(itemID === documento.id){
                   product.push(documento.data());
                   setProduct([...product]);
-                  //console.log(documento.data());
               }
             });
         });
--- src/components/items/ItemListContainer.js
+++ src/components/items/ItemListContainer.js
@@ -7,34 +7,18 @@ const ItemListContainer = (props) => {
   const [items,setItems] = useState([]);
 
   useEffect(()=>{
-    //getItems();
     const firestore = getFireStore();
-    //console.log(firestore);
     const collection = firestore.collection("ItemCollection");
-    //console.log(collection);
     const query = collection.get(); 
     query.then((resultado)=>{
-      //console.log(resultado);
-      //console.log(resultado.docs);
       resultado.forEach(documento => {
-        //console.log(documento.data());
         items.push(documento.data());
          setItems([...items]);
       });
     });
   },[]);
   
- /*const getItems = async() =>{
-    const data = await fetch('http://localhost:4000/products');
-    const responseData = await data.json();
-    return new Promise((res, rej) => {
-      setTimeout(() => {
-        res(setItems(responseData));
-      },2000);
-    });
-  }*/
-  
-  return(
+ return(
     <div>
       <div className="container-fluid">
         <div className="row">
